0c7bf2e3b6284b616f85cd03571e0d906b36c5f8
Charlie Quin <charliequin@outlook.com>
2021-12-03 00:53:52 +0000

    feat: completed task 2.a

---
+++ src/tasks/2/a/index.ts
@@ -0,0 +1,39 @@
+import data from "./data.js";
+
+const getPosition = (data: string[]) => {
+  let x = 0;
+  let y = 0;
+
+  const controls = {
+    FORWARD: "forward",
+    DOWN: "down",
+    UP: "up",
+  };
+
+  data.map((curr) => {
+    const control = curr.split(" ")[0];
+    const value = parseInt(curr.split(" ")[1]);
+
+    switch (control) {
+      case controls.FORWARD:
+        return (x += value);
+      case controls.DOWN:
+        return (y += value);
+      case controls.UP:
+        return (y -= value);
+      default:
+        return;
+    }
+  });
+
+  return { x, y };
+};
+
+const multiplyNumbers = (a: number, b: number) => {
+  return a * b;
+};
+
+const { x, y } = getPosition(data);
+const answer = multiplyNumbers(x, y);
+
+export { answer };
